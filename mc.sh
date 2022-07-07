9e79e1c82a0ef4af44faf655e863684ec7e0076a
pcnow <wixsama1@gmail.com>
2022-07-07 21:19:16 +0300

    Main test

---
+++ mc.sh
@@ -0,0 +1,27 @@
+echo "Майнкрафт сервер загружается, ждите" > $root/ip.txt
+
+# start tunnel
+mkdir -p ./logs
+touch ./logs/temp # avoid "no such file or directory"
+rm ./logs/*
+echo "Запускается ngrok на континенте $ngrok_region"
+./ngrok authtoken $ngrok_token
+touch logs/ngrok.log
+./ngrok tcp -region $ngrok_region --log=stdout 1025 > ./logs/ngrok.log &
+# wait for started tunnel message, and print each line of file as it is written
+tail -f ./logs/ngrok.log | sed '/started tunnel/ q'
+orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
+trimmed_server_ip=`echo $orig_server_ip | grep -o '[a-zA-Z0-9.]*\.ngrok.io[0-9:]*'`
+server_ip="${trimmed_server_ip:-$orig_server_ip}"
+echo "IP сервера: $server_ip"
+echo "Сервер запущен на: $server_ip" > $root/ip.txt
+
+touch logs/latest.log
+# Experiment: Run http server after all ports are opened
+#( tail -f ./logs/latest.log | sed '/RCON running on/ q' && python3 -m http.server 8080 ) &
+
+# Start minecraft
+#PATH=$PWD/jre/bin:$PATH
+echo "Сервер запущен..."
+java -Xmx1G -Xms1G -jar server.jar nogui
+echo "Код выхода $?"
---
+++ mcdl.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Версия Майнкрафта
+VERSION=1.16.3
+
+set -e
+root=$PWD
+mkdir -p mc
+cd mc
+
+#export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+#export PATH=$JAVA_HOME/bin:$PATH
+
+download() {
+    set -e
+    echo Запуская этот установщик, вы соглашаетесь с лицензионным соглашением JRE, с лицензионным соглашением Spigot,
+    echo лицензионным соглашением Geyser, с Mojang Minecraft EULA
+    echo с лицензионным соглашением NPM, лицензионным соглашением MIT,
+    echo и всеми лицензионным соглашениями, использованных в этом проекте.
+    echo Нажмите Ctrl-C и удалите данный repl, если вы не согласны.
+    echo Нажмите Enter, если вы не согласны.
+    read -s agree_text
+    echo Спасибо за согласие, Установка сейчас начнётся.
+    wget pcnow.github.io/replit-mc/mc.sh
+    echo Скрипт запуска загружен
+    wget "https://download.getbukkit.org/spigot/spigot-$VERSION.jar"
+    mv "spigot-$VERSION.jar" server.jar
+    echo Spigot загружен
+    wget https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
+    mkdir plugins
+    mv Geyser-Spigot.jar plugins/spigot.jar
+    echo Geyser загружен
+    echo "eula=true" > eula.txt
+    echo Согласен с Mojang EULA
+    wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    unzip -o ngrok.zip
+    rm -rf ngrok.zip
+    echo "Загрузка завершена" 
+}
+
+require() {
+    if [ ! $1 $2 ]; then
+        echo $3
+        echo "Загрузка запущена..."
+        download
+    fi
+}
+require_file() { require -f $1 "Файл $1 нужен, но не найден"; }
+require_dir()  { require -d $1 "Директория $1 нужна, но не найдена"; }
+require_env() {
+  if [[ -z "${!1}" ]]; then
+    echo "Переменная $1 не назначена. "
+    echo "Создайтет новый секрет(secret) названный $1 и назначте ему $2"
+    exit
+  fi
+}
+require_executable() {
+    require_file "$1"
+    chmod +x "$1"
+}
+
+# server files
+require_file "eula.txt"
+require_file "server.properties"
+require_file "server.jar"
+# java
+#require_dir "jre"
+#require_executable "jre/bin/java"
+# ngrok binary
+require_executable "ngrok"
+
+# environment variables
+require_env "ngrok_token" "ваш ngrok authtoken на https://dashboard.ngrok.com"
+require_env "ngrok_region" "ваш континент, одну из:
+us - США (Охио)
+eu - Европа (Франкфурт)
+ap - Азия (Сингапур)
+au - Австралия (Сидней)
+sa - Северная Америка (Сао Пауло)
+jp - Япония (Токио)
+in - Индия (Мумбаи)
