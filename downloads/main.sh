1ab15f6fdaa653e9e52cabb5d92fa860625f0f98
pcnow <wixsama1@gmail.com>
2022-07-07 22:00:43 +0300

    Fixed bug with echo

--- downloads/main.sh
+++ downloads/main.sh
@@ -6,7 +6,7 @@ echo          PCNow!, 2022
 echo 
 echo Если вы хотите установить Майнкрафт сервер,
 echo нажмите STOP, скопируйте данную команду:
-echo "wget pcnow.github.io/downloads/mcdl.sh && bash mcdl.sh" (без кавычек)
+echo ""wget pcnow.github.io/downloads/mcdl.sh && bash mcdl.sh" (без кавычек)"
 echo перейдите на вкладку Shell,
 echo вставьте эту команду и нажмите Enter.
 echo Если вы хотите запустить сервер,
