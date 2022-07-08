8aa20be69435efd0faef6883b0979d079d21d6fc
pcnow <wixsama1@gmail.com>
2022-07-08 13:22:42 +0300

    Test bugfix

--- downloads/main.sh
+++ downloads/main.sh
@@ -4,12 +4,27 @@ echo
 echo Загрузщик Майнкрафт сервера v1.0
 echo          PCNow!, 2022
 echo 
-echo Если вы хотите установить Майнкрафт сервер,
-echo нажмите STOP, скопируйте данную команду:
-echo ""wget pcnow.github.io/downloads/mcdl.sh && bash mcdl.sh" (без кавычек)"
-echo перейдите на вкладку Shell,
-echo вставьте эту команду и нажмите Enter.
 echo Если вы хотите запустить сервер,
-echo нажмите Enter.
-read -s
-bash mc.sh
+echo нажмите 1.
+echo Если вы хотите установить Майнкрафт сервер,
+echo нажмите 2.
+
+while true
+do
+
+read -n1 Keypress
+
+case "$Keypress" in
+
+$'1')
+echo Майнкрафт сервер загружается, ждите
+exit
+;;
+
+$'2')
+echo Начинается установка
+;;
+
+esac
+
+done
